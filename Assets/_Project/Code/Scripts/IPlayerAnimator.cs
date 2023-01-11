82a8cca8349c9a0d250419e336c2c42985e90601
Jose Palacio <jj.pc101017@gmail.com>
2023-01-11 18:50:46 -0500

    Basic animator functionality implemented

---
+++ Assets/_Project/Code/Scripts/IPlayerAnimator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerAnimator
+{
+    void SetAnimation(PlayerAnimatorManager.AnimationState animationState);
+}
