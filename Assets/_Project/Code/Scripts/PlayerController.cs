bb84d16503b8fdbe9c65d1f68e1e57502c70ecf8
Jose Palacio <jj.pc101017@gmail.com>
2023-01-11 23:30:18 -0500

    Cleaned project and implemented basic player movement

--- Assets/_Project/Code/Scripts/PlayerController.cs
+++ Assets/_Project/Code/Scripts/PlayerController.cs
@@ -1,10 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace LifeIsTheGame.TechnicalTest
 {
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour, IPlayerController
     {
+        private IInputActions _inputActions;
+
+        [SerializeField]
+        private CharacterController controller;
+
+        [SerializeField]
+        private float playerSpeed = 2.0f;
+        private Vector3 _playerVelocity;
+
+        private void Start()
+        {
+            InitializeInputActions();
+        }
+
+        private void InitializeInputActions()
+        {
+            _inputActions = GameManager.instance.inputActions;
+            _inputActions.SubscribeToEventOnMove(UpdateMovement);
+        }
+
+        private void UpdateMovement(Vector2 movement)
+        {
+            var movementV3 = new Vector3(movement.x, 0, movement.y);
+            controller.Move(movementV3 * (Time.deltaTime * playerSpeed));
+        }
+
+        private void OnDestroy()
+        {
+            if (_inputActions == null) return;
+
+            _inputActions.UnsubscribeFromEventOnMove(UpdateMovement);
+        }
     }
 }
